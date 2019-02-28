fee537e439800be9bbf0c7697c188a184ec48c80
Segev BenZvi <sybenzvi@icecube.wisc.edu>
2019-02-28 14:25:55 -0800

    Add standard DESI package structure.

---
+++ py/desidithering/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+=============
+desidithering
+=============
+
+Tools for DESI dithering simulation and reconstruction.
+"""
+from __future__ import print, absolute_import
+from ._version import __version__
